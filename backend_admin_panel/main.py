ebd79604e0406c862d4e9eb96d3991fe6f9cc0ee
aayaneth <aayandeori2@gmail.com>
2026-09-05 15:48:13 +0530

    curly-admin-panel

--- backend_admin_panel/main.py
+++ backend_admin_panel/main.py
@@ -46,7 +46,8 @@ app.add_middleware(
         "http://localhost:5500", 
         "http://127.0.0.1:5500",
         "http://localhost:5173", # Allows your local Vite frontend
-        "https://rings-photograph-cancer-spoke.trycloudflare.com" # Allows your Cloudflare tunnel
+        "https://curly-admin-panel-six.vercel.app"
+        "*" # wildcard for easy testing
     ], 
     allow_credentials=True,
     allow_methods=["*"],
