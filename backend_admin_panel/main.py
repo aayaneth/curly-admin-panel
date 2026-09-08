b9c4c78e34188a7b9950351a10498349dec76434
aayaneth <aayandeori2@gmail.com>
2026-09-08 13:40:04 +0530

    curly-admin-panel

--- backend_admin_panel/main.py
+++ backend_admin_panel/main.py
@@ -46,8 +46,7 @@ app.add_middleware(
         "http://localhost:5500", 
         "http://127.0.0.1:5500",
         "http://localhost:5173", # Allows your local Vite frontend
-        "https://curly-admin-panel-six.vercel.app"
-        "*" # wildcard for easy testing
+        "https://curly-admin-panel-six.vercel.app",
     ], 
     allow_credentials=True,
     allow_methods=["*"],
